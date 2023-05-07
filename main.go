d22c5956f61ab1f7e58a622ecb70518ff3c363a0
desarrollo-garrido <desarrollo@garrido.pro>
2023-05-07 20:56:28 +0200

    otro cambio mas

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/devgarrido/godesde0/variables"
 )
 
 func main() {
 	var1, var2 := variables.ConviertoaTexto(1209)
-	if var1 {
+	if !var1 {
 		fmt.Println("El valor es\n" + var2)
 	}
 
+	if os := runtime.GOOS; os != "windows" {
+		fmt.Println("Este sistema no es windows")
+	} else {
+		fmt.Println("Este sistema es windows")
+	}
+
+	// Vamos a probar el switch
+
+	switch os := runtime.GOOS; os {
+	case "windows":
+		fmt.Println("Esto es windows")
+	case "linux":
+		fmt.Println("Esto es linux")
+	default:
+		fmt.Printf("%s\n", os)
+	}
 }
